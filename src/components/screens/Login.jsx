e10660c4c7d5af43a01736de3676998e744d8175
GleipM <20233tn181@utez.edu.mx>
2025-04-22 03:36:58 -0600

    mejor no

--- src/components/screens/Login.jsx
+++ src/components/screens/Login.jsx
@@ -48,12 +48,8 @@ export default function Form() {
             // Set token as default Authorization header for future requests
             axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
 
-            await fetch("https://gmm0ermcb9.execute-api.us-east-1.amazonaws.com/api/ping");
-    
-            // Esperar un segundo para asegurarse de que todo está listo
-            setTimeout(() => {
-                navigate("/ventas"); // Redirigir a la ruta de ventas
-            }, 1000);
+            // Navigate to home page after successful login
+            navigate('/home');
 
         } catch (error) {
             console.error("Login error:", error);
